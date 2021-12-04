df274bf3801c42d791bf7385212177321231abf0
Louxml <1764097857@qq.com>
2021-12-04 16:42:24 +0800

    [update]修改vue项目+ElementUI

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-button type="primary">el-button</el-button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import installElementPlus from './plugins/element'
+
+const app = createApp(App)
+app.use(installElementPlus)
+app.mount('#app')
